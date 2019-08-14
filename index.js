9902aa7a10ea7c7d81abce201cc8d618ada19aa8
vincent servio <vincentservio@gmail.com>
2019-08-14 03:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-function happyHolidays (happyHolidays === Happy holidays)
+function happyHolidays (happyHolidays === "Happy holidays!")
  return "Happy holidays!"{
 }
 function happyHolidaysTo
