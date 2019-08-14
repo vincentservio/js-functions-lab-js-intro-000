8698430e4d472226d64f416578c964b3e0af30fc
vincent servio <vincentservio@gmail.com>
2019-08-14 03:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo (name){
 function happyHolidayTo (holiday,name){
   return (`Happy ${holiday}, ${name}!`)
   }
-  function holidayCountdown ()
-  return
+  function holidayCountdown (holiday,days){
+  return (`It's ${days} days until ${holiday}!`)
 }
